cd341cba95dd2d5e8876a63e5ecf3bc07a55e145
AnirudhHada <hadaanirudh10@gmail.com>
2024-11-16 12:50:40 +0530

    initial commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+	return (
+		<header>
+			<div>
+				<h1>Shop It</h1>
+			</div>
+			<nav>
+				<span class="material-symbols-outlined">favorite</span>
+				<span class="material-symbols-outlined">shopping_cart</span>
+			</nav>
+		</header>
+	);
+};
